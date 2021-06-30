2d38510d8737c541200b6a1f828dde7cb568b343
minh-p <minhisverynormal@gmail.com>
2021-06-30 15:11:47 -0700

    messing around with da countdown

--- resources/js/main.js
+++ resources/js/main.js
@@ -27,17 +27,28 @@ function betterCaculate(num1, num2, operator) {
 }
 
 const heading1 = document.querySelector("h1");
-var introString = "Calculated: ";
-var numToBeMultiplied = 3;
 
-heading1.textContent = "How";
-
-// Doing this later lol not working.
 /*
-int i;
-for (i = 0; i < 10; i++) {
-    var calculated = betterCaculate(numToBeMultiplied, 10, "*");
-    heading1.textContent = introString.concat(calculated.toString());
-    wait(1);
-}
+var calculated = betterCaculate(10, 2, "*");
+var introString = "Calculated: ";
+heading1.textContent = introString.concat(calculated.toString());
 */
+
+function congratulate() {
+    heading1.textContent = "Congrats! Event will now commence (JK there is nothing)";
+}
+
+
+var countdownFrom = 10;
+var currentCountdown = countdownFrom;
+
+var interval = setInterval(function() {
+    heading1.textContent = "Countdown: ".concat(currentCountdown.toString());
+
+    if (currentCountdown <= 0) {
+        clearInterval(interval);
+        congratulate();
+    }
+
+    currentCountdown--;
+}, 1000);
